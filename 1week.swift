63d6d5440bcf8ecac771d343ffea4df1fcedce78
최지훈 <chl020729@gmail.com>
2024-06-03 21:01:35 +0900

    1week

---
+++ 1week.swift
@@ -0,0 +1,19 @@
+
+// 1 주차
+var greeting = "Hello, playground" //자동 완성 가능
+
+print(greeting)
+
+dump(greeting)
+
+var sum = 0
+for i in 1 ... 10 {
+    sum += i
+}
+
+print(sum)
+
+
+// 주석은 슬래쉬를 2개를 연달아서 적어서 처리한다.
+// /* */
+
